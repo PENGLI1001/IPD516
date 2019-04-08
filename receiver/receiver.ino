cef2e3604502b5bcfbbb53d4a5218e660f0ff336
PENG LI <49007705+PENGLI1001@users.noreply.github.com>
2019-04-08 12:22:14 -0400

    Update receiver.ino
    add more notes

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -77,6 +77,20 @@ void process_midi_command() {
 
 int note_to_frequency(uint8_t note) {
   switch(note) {
+        case 36: return 65;
+    case 37: return 69;
+    case 38: return 73;
+    case 39: return 78;
+    case 40: return 82;
+    case 41: return 87;
+    case 42: return 93;
+    case 43: return 98;
+    case 44: return 104;
+    case 45: return 110;
+    case 46: return 117;
+    case 47: return 123;
+    case 48: return 131;
+    case 49: return 139;
     case 50: return 147;
     case 51: return 156;
     case 52: return 165;
@@ -107,6 +121,54 @@ int note_to_frequency(uint8_t note) {
     case 77: return 698;
     case 78: return 740;
     case 79: return 784;
+    case 80: return 831;
+    case 81: return 880;
+    case 82: return 932;
+    case 83: return 988;
+    case 84: return 1046;
+    case 85: return 1109;
+    case 86: return 1175;
+    case 87: return 1245;
+    case 88: return 1319;
+    case 89: return 1397;
+    case 90: return 1480;
+    case 91: return 1568;
+    case 92: return 1661;
+    case 93: return 1760;
+    case 94: return 1865;
+    case 95: return 1976;
+    case 96: return 2093;
+    case 97: return 2217;
+    case 98: return 2349;
+    case 99: return 2489;
+    case 100: return 2637;
+    case 101: return 2794;
+    case 102: return 2960;
+    case 103: return 3136;
+    case 104: return 3322;
+    case 105: return 3520;
+    case 106: return 3729;
+    case 107: return 3951;
+    case 108: return 4186;
+    case 109: return 4435;
+    case 110: return 4699;
+    case 111: return 4978;
+    case 112: return 5274;
+    case 113: return 5588;
+    case 114: return 5920;
+    case 115: return 6272;
+    case 116: return 6645;
+    case 117: return 7040;
+    case 118: return 7459;
+    case 119: return 7902;
+    case 120: return 8372;
+    case 121: return 8870;
+    case 122: return 9397;
+    case 123: return 9956;
+    case 124: return 10548;
+    case 125: return 11175;
+    case 126: return 11840;
+    case 127: return 12544;
     default: return 0;
   }
 }
